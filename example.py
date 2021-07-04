5768e48e95275acd76a234a57914f6c2765dcde0
ashleve <zalewski.ukas@gmail.com>
2021-07-04 19:52:08 +0200

    add hackernews example

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 import openai
-
 import dotenv
 import os
 
@@ -8,4 +7,24 @@ import os
 dotenv.load_dotenv(override=True)
 
 
-api_key = os.environ['OPENAI_API_KEY']
+openai.api_key = os.getenv("OPENAI_API_KEY")
+filename = 'prompts/hackernews.txt'
+engine = "davinci"
+
+
+with open(filename, "r") as file:
+    prompt = file.read()
+
+
+response = openai.Completion.create(
+    engine=engine,
+    prompt=prompt,
+    temperature=0.95,
+    max_tokens=50,
+    top_p=1.0,
+    frequency_penalty=0.9,
+    presence_penalty=0.9,
+    stop=["."],
+)
+
+print(response["choices"][0]["text"])
